c9b0390e60258365af653a51ded14742fcb78c4b
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-02-17 14:35:51 +0900

    Define filename

--- app/Main.hs
+++ app/Main.hs
@@ -8,17 +8,24 @@ import Network.Wai.Handler.WarpTLS
 import Data.Text.Lazy hiding (map)
 import Data.Text.Lazy.Encoding
 import qualified Data.CaseInsensitive as CI
+import qualified Data.ByteString.Lazy as LBS
 
 main :: IO ()
 main = runTLS (tlsSettings "localhost.pem" "localhost-key.pem") defaultSettings app
 
+images :: [LBS.ByteString]
+images =  map (\c -> encodeUtf8 $ pack ("img/" <> [c] <> ".jpg")) ['a'..'e']
+
 app :: Application
-app req respond =
+app req respond = do
+  -- let http2 = defaultHTTP2Data
+        -- { http2dataPushPromise =
+            -- map (\p -> defaultPushPromise { promisedPath }) }
   case pathInfo req of
     [] ->
       respond $ responseLBS status200 [(CI.mk "content-type", "text/html")] $
       "<html><head><title>http2warp</title><style>img{width: 100px}</style></head><body>" <>
-      (encodeUtf8 . pack . mconcat $ map (\c ->"<img src=\"img/" <> [c] <> ".jpg\">") ['a'..'e']) <>
+      mconcat (map (\p -> "<img src=\"" <> p <> "\">") images) <>
       "</body>"
     ["img", x] ->
       respond $ responseFile status200 [(CI.mk "content-type", "image/jpeg")]
