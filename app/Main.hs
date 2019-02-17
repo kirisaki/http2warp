df0a77e9c8c346693e34b4deea10c23f4b5fc906
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-02-17 14:10:36 +0900

    Basic web server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.Wai.Handler.WarpTLS
+import Data.Text.Lazy hiding (map)
+import Data.Text.Lazy.Encoding
+import qualified Data.CaseInsensitive as CI
 
 main :: IO ()
-main = someFunc
+main = runTLS (tlsSettings "localhost.pem" "localhost-key.pem") defaultSettings app
+
+app :: Application
+app req respond =
+  case pathInfo req of
+    [] ->
+      respond $ responseLBS status200 [(CI.mk "content-type", "text/html")] $
+      "<html><head><title>http2warp</title><style>img{width: 100px}</style></head><body>" <>
+      (encodeUtf8 . pack . mconcat $ map (\c ->"<img src=\"img/" <> [c] <> ".jpg\">") ['a'..'e']) <>
+      "</body>"
+    ["img", x] ->
+      respond $ responseFile status200 [(CI.mk "content-type", "image/jpeg")]
+      (unpack . fromStrict $ "./img/" <> x) Nothing
+    _ ->
+      respond $ responseLBS status404 [(CI.mk "content-type", "text/plain")]
+      "nyaan"
